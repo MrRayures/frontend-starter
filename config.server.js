9ccfe6241b210f116672dfe855603a83e5a20b03
Francois L <mrrayures@gmail.com>
2023-05-16 23:21:49 +0200

    WIP: Add live-server & test

---
+++ config.server.js
@@ -0,0 +1,18 @@
+/*
+* params : https://www.npmjs.com/package/live-server
+*/
+var liveServer = require("live-server");
+
+var params = {
+	port: 3000, // Set the server port. Defaults to 8080.
+	host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "dist/", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+	watch: [["dist/assets/css/*.css", "dist/*.html", "dist/assets/js/*.js", "dist/assets/img/**/*.{jpg,jpeg,png,svg}"]],
+	ignore: '', // comma-separated string for paths to ignore
+	file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+	wait: 0, // Waits for all changes, before reloading. Defaults to 0 sec.
+	mount: [['/components', './node_modules']], // Mount a directory to a route.
+	logLevel: 1, // 0 = errors only, 1 = some, 2 = lots
+};
+liveServer.start(params);
